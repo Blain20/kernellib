49797f72550fa6f0326f7407187c5c70ebaf8132
Raymond Jennings <shentino@gmail.com>
2016-01-10 21:35:02 -0800

    Oops

--- kernel/lib/auto.c
+++ kernel/lib/auto.c
@@ -1730,25 +1730,3 @@ nomask void _F_reset_resources()
 	resources = nil;
     }
 }
-
-/*
- * NAME:	_F_query_resources()
- * DESCRIPTION:	Return resources tracked by an object.
- */
-nomask mapping _F_query_resources()
-{
-    if (SYSTEM() && resources) {
-	return resources[..];
-    }
-}
-
-/*
- * NAME:	_F_reset_resources()
- * DESCRIPTION:	Reset resources tracked by an object.
- */
-nomask void _F_reset_resources()
-{
-    if (SYSTEM()) {
-	resources = nil;
-    }
-}
